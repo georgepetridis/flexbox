b3054b89d0091b083e8817499a4f7bb94a5072f4
George Petridis <george@georgepetridis.com>
2016-11-29 01:10:36 -0500

    First layout with just header

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require( 'gulp' );
+var browserSync = require( 'browser-sync' ).create();
+var sass = require( 'gulp-sass' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+
+// Static Server + watching scss/html files
+gulp.task( 'serve', [ 'sass' ], function () {
+
+  browserSync.init( {
+    proxy: 'flexbox.dev',
+    browser: 'firefox'
+  } );
+
+  gulp.watch( "scss/*.scss", [ 'sass' ] );
+  gulp.watch( "*.html" ).on( 'change', browserSync.reload );
+} );
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task( 'sass', function () {
+  return gulp.src( "scss/*.scss" )
+    .pipe( sass() ).on( 'error', sass.logError )
+    .pipe( autoprefixer() )
+    .pipe( gulp.dest( "css" ) )
+    .pipe( browserSync.stream() );
+} );
+
+gulp.task( 'default', [ 'serve' ] );
