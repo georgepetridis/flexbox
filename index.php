b590850c6b4b7bb27bbddc2a6d569d531c342a9c
George Petridis <george@georgepetridis.com>
2016-12-01 11:57:51 -0500

    Stylesheet path chaned to relative

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Flexbox</title>
-    <link rel="stylesheet" href="/css/main.css" media="screen">
+    <link rel="stylesheet" href="css/main.css" media="screen">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
